df5be6fcd8915822bbba5e28d0f2122c4e6b4542
Saad <dz8sot@gmail.com>
2016-03-07 11:41:08 +0100

    Update Fembed.php

--- Fembed.php
+++ Fembed.php
@@ -1,38 +1,42 @@
 <?php
 /**
- * Plugin Name: Fembed
- * Description: Embed Facebook videos easily 
- * Plugin URI:  https://github.com/s4ad/Fembed
- * Version:     0.0.1
- * Author:      Saad Bouteraa
- **/
 
-add_action( 'admin_head', 'Fembed_tinymce' );
-function Fembed_tinymce() {
+Plugin Name: fembed - Facebook videos embeder
+Plugin URI: https://github.com/s4ad/Fembed
+Description: Embed Facebook videos easily
+Version: 1.0.1
+Author: Saad Bouteraa
+Author URI: https://github.com/s4ad/
+
+ **/
+ 
+
+add_action( 'admin_head', 'fembed_tinymce' );
+function fembed_tinymce() {
     global $typenow;
 
     // only on Post Type: post and page
     if( ! in_array( $typenow, array( 'post', 'page' ) ) )
         return ;
 
-    add_filter( 'mce_external_plugins', 'Fembed_tinymce_plugin' );
+    add_filter( 'mce_external_plugins', 'fembed_tinymce_plugin' );
     // Add to line 1 form WP TinyMCE
-    add_filter( 'mce_buttons', 'Fembed_tinymce_button' );
+    add_filter( 'mce_buttons', 'fembed_tinymce_button' );
 }
 
 // inlcude the js for tinymce
-function s4_embed_tinymce_plugin( $plugin_array ) {
+function fembed_tinymce_plugin( $plugin_array ) {
 
-    $plugin_array['Fembed'] = plugins_url( '/Fembed.js', __FILE__ );
-    // Print all plugin js path
+    $plugin_array['fembed'] = plugins_url( '/fembed.js', __FILE__ );
+    
     
     return $plugin_array;
 }
 
 // Add the button key for address via JS
-function Fembed_tinymce_button( $buttons ) {
+function fembed_tinymce_button( $buttons ) {
 
-    array_push( $buttons, 'Fembed_button_key' );
+    array_push( $buttons, 'fembed_button_key' );
     // Print all buttons
     
     return $buttons;
